4f5978f9e2c85cad6e025dfb84343e06dc84d5d0
Wensenter <18365206169@163.com>
2020-06-21 23:00:40 +0800

    update

--- js/login.js
+++ js/login.js
@@ -3,3 +3,12 @@ function checkoutLogin(el){
     let index = $(el).index()
     $('form').eq(index).show().siblings('form').hide()
 }
+
+function checkoutPsw(el){
+    $(el).hide().siblings('.psw-pic').show()
+    if($(el).hasClass('hide-psw')){
+        $('.psw').attr('type','text')
+    }else if($(el).hasClass('show-psw')){
+        $('.psw').attr('type','password')
+    }
+}
